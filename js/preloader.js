d17ff16f1174cbfd76f4a3baf8fc5120f69a9875
vgabrielk <vfelix1553@gmail.com>
2022-01-04 03:03:02 -0300

    curriculo

---
+++ js/preloader.js
@@ -0,0 +1,10 @@
+let loader = document.querySelector('#preloader');
+
+window.addEventListener("load", function(){
+    let preloaderDelay = setTimeout(function() {
+        loader.style.display ="none"
+      }, 1000)
+    
+    
+})
+
